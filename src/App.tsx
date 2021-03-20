02827b6fd9ec394c88f9459465406f29a8a2653d
AliHaiderrr <gamicacloudgit0@gmail.com>
2021-03-20 17:23:20 +0500

    timer app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Main from './Components/Main/Main';
+import MainHeading from './Components/MainHeading';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   <MainHeading/>
+   <Main/>
+    </div>
+  );
+}
+
+export default App;
